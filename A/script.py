e5cff7a7d0666843587f8452bd2b5f425ee8a3e6
Pavel Shibaev <shibaev.pp@phystech.edu>
2018-03-20 01:13:12 +0300

    Uploaded homework

---
+++ A/script.py
@@ -0,0 +1,3 @@
+header = open("index.h", "w")
+print("#include<iostream>\n#include<vector>", file=header)
+
---
+++ main.cpp
@@ -0,0 +1,5 @@
+int main() {
+	std::vector<int> a = {1, 2, 3, 4};
+	for (auto i: a)
+		std::cout << i << "\n";
+}
